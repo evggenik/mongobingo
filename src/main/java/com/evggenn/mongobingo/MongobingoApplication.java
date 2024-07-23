c84dba185153cd542fa0a1b1a61ddef1291e59e5
Evgeny Buchelnikov <gru554@yandex.ru>
2024-07-23 15:56:03 +1000

    created mongobingo project

---
+++ src/main/java/com/evggenn/mongobingo/MongobingoApplication.java
@@ -0,0 +1,13 @@
+package com.evggenn.mongobingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongobingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongobingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evggenn/mongobingo/MongobingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evggenn.mongobingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongobingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
